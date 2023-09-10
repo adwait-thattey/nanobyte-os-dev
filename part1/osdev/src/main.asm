cd32cad69b4757da2d89195fd89fab2c0715d122
Adwait Thattey <coderdude1999@gmail.com>
2023-09-11 00:09:24 +0530

    Printing character stream to screen

--- part1/osdev/src/main.asm
+++ part1/osdev/src/main.asm
@@ -1,6 +1,36 @@
 org 0x7C00  ; The bios loads the code at location 7C00 so tell the assembler to set this as the offset so that remaining addresses will start from here
-
 bits 16  ; Tells the assembler to emit 16 bit code. We can use 16/32/64. But all x86 CPUs are backward compatible. 16bit is easier to write
+
+%define ENDL 0x0D, 0x0A
+
+start:
+	jmp main ;ensure that main is the entry point of our function
+
+;
+; Print a string tp the screen
+; Parameters: DS:SI are expected to contain the start of the string buffer to print
+;
+puts:
+	; save registers we will modify
+	push si
+	push ax
+
+.loop:
+	lodsb		; loads byte from ds:si into al and increments 1 byte
+	or al, al	; if al contains 0, flag will be set which helps us check null condition
+	jz .done
+
+	mov ah, 0x0e  	; handle interrupt to print to tty
+	mov bh, 0		; set page to 0
+	int 0x10		; raise video interrupt
+
+	jmp .loop
+
+.done:
+
+	pop ax
+	pop si
+	ret
 
 main:
 
@@ -15,6 +45,10 @@ main:
 	mov ss, ax
 	mov sp, 0x7C00
 
+	; print the message. LOad address in ds:si and call the puts function
+	mov si, msg_hello
+	call puts
+	call puts
 
 	hlt ; make the cpu halt. 
 
@@ -23,6 +57,7 @@ main:
 .halt:
 	jmp .halt
 
+msg_hello: db 'Hello world!', ENDL, 0		;; db writes to the memory and puts label msg_hello as reference to the memory
 
 ; For signature BIOS expects last 2 bytes in 1st sector to be AA55
 ; We are using floppy format here which has 512 bytes in 1 sector. We will fill up 510 bytes then add the signature
