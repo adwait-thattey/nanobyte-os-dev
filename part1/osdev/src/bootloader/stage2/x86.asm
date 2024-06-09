df2862d9cb50b87aa47aadd32684f19e59de4a63
Adwait Thattey <coderdude1999@gmail.com>
2024-06-09 13:46:02 +0530

    Implement multiply func

--- part1/osdev/src/bootloader/stage2/x86.asm
+++ part1/osdev/src/bootloader/stage2/x86.asm
@@ -30,6 +30,30 @@ __U4D:
     mov edx, eax
     shr edx, 16
 
+;
+; U4M
+; Operation:      integer four byte multiply
+; Inputs:         DX;AX   integer M1
+;                 CX;BX   integer M2
+; Outputs:        DX;AX   product
+; Volatile:       CX, BX destroyed
+;
+global __U4M
+__U4M:
+    shl edx, 16         ; dx to upper half of edx
+    mov dx, ax          ; m1 in edx
+    mov eax, edx        ; m1 in eax
+
+    shl ecx, 16         ; cx to upper half of ecx
+    mov cx, bx          ; m2 in ecx
+
+    mul ecx             ; result in edx:eax (we only need eax)
+    mov edx, eax        ; move upper half to dx
+    shr edx, 16
+
+    ret
+
+
 global _x86_div64_32
 _x86_div64_32:
 
