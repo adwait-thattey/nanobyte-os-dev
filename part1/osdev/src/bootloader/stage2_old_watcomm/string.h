7de62c030c2aa7dd6d16685b45f362753f1f8240
Adwait Thattey <coderdude1999@gmail.com>
2024-08-17 22:58:52 +0530

    part 7: Remove old stage2

--- part1/osdev/src/bootloader/stage2_old_watcomm/string.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// strchr finds a character in a given string
-const char* strchr(const char* str, char chr);
-char* strcpy(char* dst, const char* src);
-unsigned strlen(const char* str);
